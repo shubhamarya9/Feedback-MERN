5fb06e17a01c8bde4d3f493cbcd1d4566f4a0bd2
shubham arya <shubhamarya@shubhams-MacBook-Pro.local>
2018-09-11 14:08:09 -0400

    Authentication complete

---
+++ server/config/prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+
+  cookieKey: process.env.COOKIE_KEY
+};
+
+
+mongodb://shubhamarya:password1@ds153552.mlab.com:53552/feedback-prod
+
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,17 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -9,10 +20,15 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile:", profile);
-      console.log("done", done);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
