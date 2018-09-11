6052335b6107baf87566a068b442764a5fa74edc
shubham arya <shubhamarya@shubhams-MacBook-Pro.local>
2018-09-11 15:02:58 -0400

    changes

--- server/services/passport.js
+++ server/services/passport.js
@@ -17,8 +17,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL:
-        "https://stormy-escarpment-93154.herokuapp.com/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
